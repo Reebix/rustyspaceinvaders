e876a561b03bbf5e6978d41889df1b06cce61cb1
Rebix <timon@set-richter.de>
2025-01-05 16:40:47 +0100

    a little functioning version

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 // hide console
-#![windows_subsystem = "windows"]
-
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 mod drawings;
 
-use crate::drawings::{get_bullet, get_invader, get_player};
+use crate::drawings::{get_bullet, get_invader, get_number, get_player};
 use minifb::{Key, Window, WindowOptions};
+use rand::random;
 
 const WIDTH: usize = 480;
 const HEIGHT: usize = 450;
@@ -32,12 +32,18 @@ fn main() {
     let player_speed = 4;
     let bullet_speed = 8;
     let mut invader_speed: isize = 1;
+    let time_to_shoot = 60;
+    let mut time = 0;
+    let mut lives = 3;
+    let mut iteration = 1;
+    let mut score = 0;
 
-    let mut invader_pos = (30, 30);
+    let mut invader_pos: (usize, usize) = (30, 30);
     let mut player_pos = (WIDTH / 2, HEIGHT - 50);
 
     let mut bullets = vec![];
     let mut invaders = [true; 12 * 4];
+    let mut invader_bullets = vec![];
 
     let mut window = Window::new(
         "Rusty Space Invaders",
@@ -51,7 +57,27 @@ fn main() {
 
     window.set_target_fps(60);
 
-    while window.is_open() && !window.is_key_down(Key::Escape) && invaders.iter().any(|&x| x) {
+    let mut finished = false;
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        if lives == 0 {
+            if !finished {
+                buffer = vec![0; WIDTH * HEIGHT];
+                for (i, c) in score.to_string().chars().enumerate() {
+                    insert_drawing(
+                        &mut buffer,
+                        i * get_number('0').width + WIDTH / 2
+                            - get_number('0').width * score.to_string().len() / 2,
+                        WIDTH / 2,
+                        &get_number(c),
+                    );
+                }
+                finished = true;
+            }
+            window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
+            continue;
+        }
+
         // clear the buffer
         for i in buffer.iter_mut() {
             *i = 0;
@@ -77,14 +103,54 @@ fn main() {
             );
         });
 
+        // draw invader bullets
+        invader_bullets
+            .iter_mut()
+            .for_each(|bullet: &mut (isize, isize)| {
+                insert_drawing(
+                    &mut buffer,
+                    bullet.0 as usize,
+                    bullet.1 as usize,
+                    &get_bullet(),
+                );
+            });
+
+        // update invader bullets
+        let mut new_invader_bullets = vec![];
+        invader_bullets
+            .iter_mut()
+            .for_each(|bullet: &mut (isize, isize)| {
+                let new_bullet = (bullet.0, bullet.1 + bullet_speed);
+                if new_bullet.1 < HEIGHT as isize {
+                    new_invader_bullets.push(new_bullet);
+                }
+            });
+        invader_bullets = new_invader_bullets;
+
         // draw player
         insert_drawing(&mut buffer, player_pos.0, player_pos.1, &get_player());
 
         // update invaders
-        invader_pos.0 += invader_speed;
-        if invader_pos.0 > (WIDTH - 12 * 30) as isize || invader_pos.0 < 0 {
+        invader_pos.0 = (invader_pos.0 as isize + invader_speed).max(10) as usize;
+        if invader_pos.0 > (WIDTH - 12 * 30) || invader_pos.0 == 10 {
             invader_speed = -invader_speed;
             invader_pos.1 += 10;
+        }
+
+        if time % (time_to_shoot / iteration) == 0 {
+            // randomly select with rand::random
+            let index = random::<u8>() % 12;
+
+            // get highest invader in the column
+            for j in (0..4).rev() {
+                if invaders[index as usize * 4 + j] {
+                    invader_bullets.push((
+                        (invader_pos.0 + index as usize * 30) as isize,
+                        (invader_pos.1 + j * 30) as isize,
+                    ));
+                    break;
+                }
+            }
         }
 
         // draw invaders
@@ -93,7 +159,7 @@ fn main() {
                 if invaders[i * 4 + j] {
                     insert_drawing(
                         &mut buffer,
-                        (invader_pos.0 + (i * 30) as isize) as usize,
+                        (invader_pos.0 + (i * 30)),
                         invader_pos.1 + j * 30,
                         &get_invader(),
                     );
@@ -107,13 +173,13 @@ fn main() {
             let mut hit = false;
             for i in 0..12 {
                 for j in 0..4 {
-                    let invader_x = (invader_pos.0 + (i * 30) as isize) as usize;
+                    let invader_x = invader_pos.0 + (i * 30);
                     let invader_y = invader_pos.1 + j * 30;
                     if invaders[i * 4 + j]
                         && bullet.0 >= invader_x
                         && bullet.0 <= invader_x + 24
                         && bullet.1 >= invader_y
-                        && bullet.1 <= invader_y + 18
+                        && bullet.1 <= invader_y + 24
                     {
                         invaders[i * 4 + j] = false;
                         hit = true;
@@ -121,6 +187,7 @@ fn main() {
                     }
                 }
                 if hit {
+                    score += iteration * 10;
                     break;
                 }
             }
@@ -129,6 +196,47 @@ fn main() {
             }
         }
         bullets = remaining_bullets;
+
+        // draw lives
+        for i in 0..lives {
+            insert_drawing(&mut buffer, WIDTH - (40 + i * 30), 10, &get_player());
+        }
+
+        // draw score
+        for (i, c) in score.to_string().chars().enumerate() {
+            insert_drawing(
+                &mut buffer,
+                10 + i * get_number('0').width,
+                10,
+                &get_number(c),
+            );
+        }
+
+        // check for collisions with player
+        let mut remaining_invader_bullets = vec![];
+        for bullet in invader_bullets.iter() {
+            let mut hit = false;
+            if bullet.0 >= player_pos.0 as isize
+                && bullet.0 <= player_pos.0 as isize + 24
+                && bullet.1 >= player_pos.1 as isize
+                && bullet.1 <= player_pos.1 as isize + 24
+            {
+                hit = true;
+                lives -= 1;
+            }
+            if !hit {
+                remaining_invader_bullets.push(*bullet);
+            }
+        }
+        invader_bullets = remaining_invader_bullets;
+
+        // when all invaders are gone, reset the game
+        if invaders.iter().all(|&x| !x) {
+            invaders = [true; 12 * 4];
+            invader_pos = (30, 30);
+            bullets = vec![];
+            iteration += 1;
+        }
 
         if window.is_key_pressed(Key::Space, minifb::KeyRepeat::No) {
             bullets.push((player_pos.0, player_pos.1));
@@ -142,6 +250,7 @@ fn main() {
             player_pos.0 += player_speed;
         }
 
+        time += 1;
         window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
     }
 }
